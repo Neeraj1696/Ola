32634ce7df502cf584a40f95d404b7d5ef2b406b
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-04-26 22:32:35 +0530

    initial commit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,29 +3,25 @@ import "./Nav.css";
 
 function Nav() {
   return (
-    <div className="nav">
-      <div className="navbar">
-        <img
-          src="https://www.olacabs.com/webstatic/img/ola-logo.svg"
-          alt="Error Logo"
-        />
+    <>
+      <div className="navbar-left">
+        <a href="">
+          <img
+            src="https://www.olacabs.com/webstatic/img/ola-logo.svg"
+            alt="logo image"
+          />
+        </a>
       </div>
-      <div className="navbar_link">
-        <div className="navbar-item">
-          <a href="/">Ola Drive</a>
-        </div>
-        <div className="navbar-item">
-          <a href="/">Ola Select</a>
-        </div>
-        <div className="navbar-item">
-          <a href="/">Ola Fleet</a>
-        </div>
-        <div className="navbar-item">
-          <a href="/">Features</a>
-        </div>
-        <button>Book Know</button>
+      <div className="navbar-right">
+        <a href="/">Ola Drive</a>
+        <a href="/">Ola Select</a>
+        <a href="/">Ola Fleet</a>
+        <a href="/">Features</a>
+        <a href="">
+          <button>Book Now</button>
+        </a>
       </div>
-    </div>
+    </>
   );
 }
 
